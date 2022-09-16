6857692c73b2ae69bf14ca8cff0930b9a3f7cc1a
liyao <liyao@17paipai.cn>
2022-09-16 09:52:18 +0800

    init

---
+++ cmd/kratos-news-system/wire.go
@@ -0,0 +1,22 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"kratos-news-system/internal/biz"
+	"kratos-news-system/internal/conf"
+	"kratos-news-system/internal/data"
+	"kratos-news-system/internal/server"
+	"kratos-news-system/internal/service"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
