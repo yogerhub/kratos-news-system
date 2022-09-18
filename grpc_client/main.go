c21ab28451a3f4993d9e2308f5f60bc8f9c33b7b
liyao <liyao@17paipai.cn>
2022-09-18 18:37:23 +0800

    代码优化

--- grpc_client/main.go
+++ grpc_client/main.go
@@ -7,26 +7,29 @@ import (
 
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/transport/grpc"
+	kgrpc "github.com/go-kratos/kratos/v2/transport/grpc"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	callGetUserByPhoneGRPC()
-	callGetArticleGRPC()
-	callGetCommentsGRPC()
-}
-func callGetUserByPhoneGRPC() {
-	conn, err := grpc.DialInsecure(
+	conn, err := kgrpc.DialInsecure(
 		context.Background(),
-		grpc.WithEndpoint("127.0.0.1:9000"),
-		grpc.WithMiddleware(
+		kgrpc.WithEndpoint("127.0.0.1:9000"),
+		kgrpc.WithMiddleware(
 			recovery.Recovery(),
 		),
 	)
+
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
+
+	callGetUserByPhoneGRPC(conn)
+	callGetArticleGRPC(conn)
+	callGetCommentsGRPC(conn)
+}
+func callGetUserByPhoneGRPC(conn *grpc.ClientConn) {
 	client := v1.NewNewsClient(conn)
 	reply, err := client.GetUserByPhone(context.Background(), &v1.GetUserByPhoneRequest{Phone: "13211112222"})
 	if err != nil {
@@ -39,18 +42,7 @@ func callGetUserByPhoneGRPC() {
 	}
 }
 
-func callGetArticleGRPC() {
-	conn, err := grpc.DialInsecure(
-		context.Background(),
-		grpc.WithEndpoint("127.0.0.1:9000"),
-		grpc.WithMiddleware(
-			recovery.Recovery(),
-		),
-	)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
+func callGetArticleGRPC(conn *grpc.ClientConn) {
 	client := v1.NewNewsClient(conn)
 	reply, err := client.GetArticle(context.Background(), &v1.GetArticleRequest{Id: 1})
 	if err != nil {
@@ -63,18 +55,7 @@ func callGetArticleGRPC() {
 	}
 }
 
-func callGetCommentsGRPC() {
-	conn, err := grpc.DialInsecure(
-		context.Background(),
-		grpc.WithEndpoint("127.0.0.1:9000"),
-		grpc.WithMiddleware(
-			recovery.Recovery(),
-		),
-	)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
+func callGetCommentsGRPC(conn *grpc.ClientConn) {
 	client := v1.NewNewsClient(conn)
 	reply, err := client.GetComments(context.Background(), &v1.GetCommentRequest{ArticleId: 1})
 	if err != nil {
